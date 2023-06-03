b7e186588159012c8b695473a6af35d7f4a71166
Giovanni <gtpepi@proton.me>
2023-06-03 18:14:10 -0300

    Add CPU counter

--- src/hardinfo.c
+++ src/hardinfo.c
@@ -2,26 +2,48 @@
 #include <string.h>
 #include "../include/open_file.h"
 
-#define MAX_LINE_LENGTH 256
+#define MAX_LINE_LENGTH 64
+#define MIN_BUFF_LENGTH 128 
+#define MAX_WORD_LENGTH 256
 
-char line[MAX_LINE_LENGTH];
-char cpu_name[MAX_LINE_LENGTH];
+void get_cpu_name(char *cpu_name, FILE* cpuinfo_file) {
+        char buff[MIN_BUFF_LENGTH];
 
-void get_cpu_info(char *cpu_name, FILE* cpuinfo_file) {
-        printf("Getting /proc/cpuinfo ...\n");
-
-        while (fgets(line, sizeof(line), cpuinfo_file)) {
-                if (strncmp(line, "model name", 10) == 0) {
-                        sscanf(line, "%*[^:]:%[^\n]", cpu_name); /* Skip the characters before the : */
+        while (fgets(buff, sizeof(buff), cpuinfo_file)) {
+                if (strncmp(buff, "model name", 10) == 0) {
+                        sscanf(buff, "%*[^:]:%[^\n]", cpu_name); /* Skip the characters before the : */
                         break;      
                 }
         }
 }
 
-void hardinfo() {        
+void get_cpu_count(int *cpu_count, FILE* cpuinfo_file) {
+        char buff[MAX_WORD_LENGTH];        
+        char sword[] = "processor";
+
+        /* Necessary to read the entire file again*/
+        rewind(cpuinfo_file); 
+
+        /* fscanf == 1 means successful reading*/
+        while (fscanf(cpuinfo_file, "%s", buff) == 1) {
+                if(strcmp(buff, sword) == 0){
+                        (*cpu_count)++;
+                }
+        }
+}
+
+void hardinfo() {     
+
+        char cpu_name[MAX_LINE_LENGTH];
+        int cpu_count = 0;
+
         FILE* cpuinfo_file = open_file("/proc/cpuinfo", "r"); 
 
-        get_cpu_info(cpu_name, cpuinfo_file);
+        get_cpu_name(cpu_name, cpuinfo_file);
+        
+        get_cpu_count(&cpu_count, cpuinfo_file);
 
         printf("CPU name: %s\n", cpu_name);
+        printf("CPU Count: %d\n", cpu_count);
+
 }
--- src/open_file.c
+++ src/open_file.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+/* Reusable file opener for /proc files*/
 FILE* open_file(const char* filename, const char* mode) {
+    printf("Opening file: %s\n", filename);
+    
     FILE* file = fopen(filename, mode);
     if (file == NULL) {
         printf("Failed to open the file.\n");
