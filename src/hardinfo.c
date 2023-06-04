fe679bdf88a3ba82495e49769cf3efc65338fcb4
Giovanni <gtpepi@proton.me>
2023-06-03 23:52:03 -0300

    Add frequencys

--- src/hardinfo.c
+++ src/hardinfo.c
@@ -42,7 +42,7 @@ void get_core_count(int *core_count, FILE* cpuinfo_file) {
         rewind(cpuinfo_file); 
 
         while (fgets(buff, sizeof(buff), cpuinfo_file)) {
-                if (strncmp(buff, "cpu cores", 9) == 0) {
+                if (strstr(buff, "cpu cores")) {
                         sscanf(buff, "%*[^:]:%[^\n]", score_count); /* Skip the characters before the : */
                         break;      
                 }
@@ -51,22 +51,47 @@ void get_core_count(int *core_count, FILE* cpuinfo_file) {
         *(core_count) = atoi(score_count);
 }
 
+void get_cpu_mhz(double **cpu_mhz, FILE* cpuinfo_file) {
+    char buff[MAX_WORD_LENGTH];
+    char tcpu_mhz[MAX_LINE_LENGTH];
+    int i = 0;
+
+    rewind(cpuinfo_file);
+
+        while (fgets(buff, sizeof(buff), cpuinfo_file)) {
+                if (strstr(buff, "MHz")) {
+                        sscanf(buff, "%*[^:]:%[^\n]", tcpu_mhz);
+                        (*cpu_mhz)[i] = strtod(tcpu_mhz, NULL);
+                        i++;
+                }
+        }
+}
 
 void hardinfo() {     
 
         char cpu_name[MAX_LINE_LENGTH];
         int proc_count = 0;
-        int core_count;
+        int core_count = 0;
+        int i = 0;
+        double *cpu_mhz = NULL;
 
         FILE* cpuinfo_file = open_file("/proc/cpuinfo", "r"); 
 
-        get_cpu_name(cpu_name, cpuinfo_file);
-        
+        get_cpu_name(cpu_name, cpuinfo_file);        
         get_proc_count(&proc_count, cpuinfo_file);
-
         get_core_count(&core_count, cpuinfo_file);
 
+        cpu_mhz = calloc(proc_count, sizeof(double));
+        get_cpu_mhz(&cpu_mhz, cpuinfo_file);
+
+        /* Print region*/
         printf("%s\n", cpu_name);
         printf("Processors: %d, Cores: %d\n", proc_count, core_count);
+
+        for (i = 0; i < proc_count; i++) {
+                printf("Processor %d - %.0f MHz\n", i, cpu_mhz[i]);
+        }
+        
+        free(cpu_mhz);
 
 }
