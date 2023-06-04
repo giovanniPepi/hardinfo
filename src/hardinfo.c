873835bbaa5253c6c149ed569ab0073b2bbae3a1
Giovanni <gtpepi@proton.me>
2023-06-04 16:13:03 -0300

    Add min/max values

--- src/hardinfo.c
+++ src/hardinfo.c
@@ -50,19 +50,43 @@ void get_core_count(int *core_count, FILE* cpuinfo_file) {
         *(core_count) = atoi(score_count);
 }
 
-void get_cpu_mhz(double **cpu_mhz, FILE* cpuinfo_file) {
+void get_cpu_mhz(double **cpu_mhz, double **min_cpu_mhz, double **max_cpu_mhz, FILE* cpuinfo_file) {
     char buff[BUFF_LENGTH];
     char tcpu_mhz[MAX_WORD_LENGTH];
     int i = 0;
-
+    double min = 0;
+    double current = 0;
+    double max = 0;
     rewind(cpuinfo_file);
 
         while (fgets(buff, sizeof(buff), cpuinfo_file)) {
                 if (strstr(buff, "MHz")) {
                         sscanf(buff, "%*[^:]:%[^\n]", tcpu_mhz);
-                        (*cpu_mhz)[i] = strtod(tcpu_mhz, NULL);
+                        (*cpu_mhz)[i] = strtod(tcpu_mhz, NULL);                        
+
+                        /* Historical data set: */
+                        current = (*cpu_mhz)[i];
+                        min = (*min_cpu_mhz)[i];
+                        max = (*max_cpu_mhz)[i];
+
+                        if (current < min) {
+                                (*min_cpu_mhz)[i] = current;
+                        }   
+                        
+                        if(current > max) {
+                                (*max_cpu_mhz)[i] = current;
+                        }
+
                         i++;
                 }
+        }
+}
+
+void fill_min_freq_values (double **min_cpu_mhz, int proc_count) {
+        
+        int i;
+        for (i = 0; i < proc_count; i++) {
+                (*min_cpu_mhz)[i] = 10000;
         }
 }
 
@@ -72,7 +96,10 @@ void hardinfo() {
         int proc_count = 0;
         int core_count = 0;
         int i = 0;
+        int j = 0;         
         double *cpu_mhz = NULL;
+        double *min_cpu_mhz = NULL;
+        double *max_cpu_mhz = NULL;
 
         FILE* cpuinfo_file = open_file("/proc/cpuinfo", "r"); 
 
@@ -80,8 +107,18 @@ void hardinfo() {
         get_proc_count(&proc_count, cpuinfo_file);
         get_core_count(&core_count, cpuinfo_file);
 
-        cpu_mhz = calloc(proc_count, sizeof(double));
-        get_cpu_mhz(&cpu_mhz, cpuinfo_file);
+        /* Mallocs*/
+        cpu_mhz = calloc(proc_count, sizeof(double));                
+        min_cpu_mhz = calloc(proc_count, sizeof(double));
+        max_cpu_mhz = calloc(proc_count, sizeof(double));
+
+        /* Fill start values with something bigger to compare */
+        fill_min_freq_values(&min_cpu_mhz, proc_count);
+
+        while (j < 5000) {
+                get_cpu_mhz(&cpu_mhz, &min_cpu_mhz, &max_cpu_mhz, cpuinfo_file);
+                j++;
+        }
 
         /* Print region*/
         printf("%s\n", cpu_name);
@@ -89,8 +126,9 @@ void hardinfo() {
         printf("[Frequencies] \n");
 
         for (i = 0; i < proc_count; i++) {
-                printf("Processor %d - %.0f MHz\n", i, cpu_mhz[i]);
+                printf("Processor [%d] Current %.0f MHz || Min %.0f || Max %.0f \n", i, cpu_mhz[i], min_cpu_mhz[i], max_cpu_mhz[i]);
         }
+
         
         free(cpu_mhz);
 
