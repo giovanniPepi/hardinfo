8fb5e8c660ecf5f9c0fcb9eef7552aa0936e9553
Giovanni <gtpepi@proton.me>
2023-06-04 23:24:31 -0300

    Update var names, update loops

--- src/hardinfo.c
+++ src/hardinfo.c
@@ -7,7 +7,7 @@
 #define BUFF_LENGTH 128 
 #define MAX_WORD_LENGTH 64
 
-void get_cpu_name(char *cpu_name, FILE* cpuinfo_file) {
+void g_cpu(char *cpu_name, FILE* cpuinfo_file) {
         char buff[BUFF_LENGTH];
 
         /* Without rewind, any other function that used the stream may have moved the start pos */
@@ -21,7 +21,7 @@ void get_cpu_name(char *cpu_name, FILE* cpuinfo_file) {
         }
 }
 
-void get_proc_count(int *proc_count, FILE* cpuinfo_file) {
+void g_proc(int *pcount, FILE* cpuinfo_file) {
         char buff[BUFF_LENGTH];        
         char sword[] = "processor";
 
@@ -30,30 +30,30 @@ void get_proc_count(int *proc_count, FILE* cpuinfo_file) {
         /* fscanf == 1 means successful reading*/
         while (fscanf(cpuinfo_file, "%s", buff) == 1) {
                 if(strcmp(buff, sword) == 0){
-                        (*proc_count)++;
+                        (*pcount)++;
                 }
         }
 }
 
-void get_core_count(int *core_count, FILE* cpuinfo_file) {
+void g_cc(int *cc, FILE* cpuinfo_file) {
         char buff[BUFF_LENGTH];        
-        char score_count[MAX_WORD_LENGTH];
+        char scc[MAX_WORD_LENGTH];
         
         rewind(cpuinfo_file); 
 
-        while (fgets(buff, sizeof(buff), cpuinfo_file)) {
+        while (fgets(buff, sizeof(buff), cpuinfo_file)) {                
                 if (strstr(buff, "cpu cores")) {
-                        sscanf(buff, "%*[^:]:%[^\n]", score_count); /* Skip the characters before the : */
+                        sscanf(buff, "%*[^:]:%[^\n]", scc); /* Skip the characters before the : */
                         break;      
                 }
         }
 
-        *(core_count) = atoi(score_count);
+        *(cc) = atoi(scc);
 }
 
-void get_cpu_mhz(double **cpu_mhz, double **min_cpu_mhz, double **max_cpu_mhz, FILE* cpuinfo_file) {
+void get_cpuf(double **cpuf, double **min_cpuf, double **max_cpuf, FILE* cpuinfo_file) {
     char buff[BUFF_LENGTH];
-    char tcpu_mhz[MAX_WORD_LENGTH];
+    char tcpuf[MAX_WORD_LENGTH];
     int i = 0;
     double min = 0;
     double current = 0;
@@ -62,20 +62,20 @@ void get_cpu_mhz(double **cpu_mhz, double **min_cpu_mhz, double **max_cpu_mhz, F
 
         while (fgets(buff, sizeof(buff), cpuinfo_file)) {
                 if (strstr(buff, "MHz")) {
-                        sscanf(buff, "%*[^:]:%[^\n]", tcpu_mhz);
-                        (*cpu_mhz)[i] = strtod(tcpu_mhz, NULL);                        
+                        sscanf(buff, "%*[^:]:%[^\n]", tcpuf);
+                        (*cpuf)[i] = strtod(tcpuf, NULL);                        
 
                         /* Historical data set: */
-                        current = (*cpu_mhz)[i];
-                        min = (*min_cpu_mhz)[i];
-                        max = (*max_cpu_mhz)[i];
+                        current = (*cpuf)[i];
+                        min = (*min_cpuf)[i];
+                        max = (*max_cpuf)[i];
 
                         if (current < min) {
-                                (*min_cpu_mhz)[i] = current;
+                                (*min_cpuf)[i] = current;
                         }   
                         
                         if(current > max) {
-                                (*max_cpu_mhz)[i] = current;
+                                (*max_cpuf)[i] = current;
                         }
 
                         i++;
@@ -83,58 +83,56 @@ void get_cpu_mhz(double **cpu_mhz, double **min_cpu_mhz, double **max_cpu_mhz, F
         }
 }
 
-void fill_min_freq_values (double **min_cpu_mhz, int proc_count) {
+void fill_min_freq_values (double **min_cpuf, int pcount) {
         
         int i;
-        for (i = 0; i < proc_count; i++) {
-                (*min_cpu_mhz)[i] = 10000;
+
+        for (i = 0; i < pcount; i++) {
+                (*min_cpuf)[i] = 10000;
         }
 }
 
 void hardinfo() {     
 
         char cpu_name[MAX_WORD_LENGTH];
-        int proc_count = 0;
-        int core_count = 0;
+        int pcount = 0;
+        int cc = 0;
         int i = 0;
         int seconds = 5;
-        double *cpu_mhz = NULL;
-        double *min_cpu_mhz = NULL;
-        double *max_cpu_mhz = NULL;
+        double *cpuf = NULL;
+        double *min_cpuf = NULL;
+        double *max_cpuf = NULL;
 
         FILE* cpuinfo_file = open_file("/proc/cpuinfo", "r"); 
 
-        get_cpu_name(cpu_name, cpuinfo_file);        
-        get_proc_count(&proc_count, cpuinfo_file);
-        get_core_count(&core_count, cpuinfo_file);
+        g_cpu(cpu_name, cpuinfo_file);        
+        g_proc(&pcount, cpuinfo_file);
+        g_cc(&cc, cpuinfo_file);
 
         /* Mallocs*/
-        cpu_mhz = calloc(proc_count, sizeof(double));                
-        min_cpu_mhz = calloc(proc_count, sizeof(double));
-        max_cpu_mhz = calloc(proc_count, sizeof(double));
+        cpuf = calloc(pcount, sizeof(double));                
+        min_cpuf = calloc(pcount, sizeof(double));
+        max_cpuf = calloc(pcount, sizeof(double));
 
         /* Fill start values with something bigger to compare */
-        fill_min_freq_values(&min_cpu_mhz, proc_count);
+        fill_min_freq_values(&min_cpuf, pcount);
 
         clock_t start_time = clock();
 
         printf("Collecting data for %d seconds...\n", seconds);
-        while((clock() - start_time) / CLOCKS_PER_SEC < seconds){
-                
-                get_cpu_mhz(&cpu_mhz, &min_cpu_mhz, &max_cpu_mhz, cpuinfo_file);
 
-        }
+        do {                
+                get_cpuf(&cpuf, &min_cpuf, &max_cpuf, cpuinfo_file);
+        } while((clock() - start_time) / CLOCKS_PER_SEC < seconds);
 
         /* Print region*/
         printf("%s\n", cpu_name);
-        printf("Processors: %d, Cores: %d\n", proc_count, core_count);
+        printf("Processors: %d, Cores: %d\n", pcount, cc);
         printf("[Frequencies] \n");
 
-        for (i = 0; i < proc_count; i++) {
-                printf("Processor [%d] Current %.0f MHz || Max %.0f || Min %.0f  \n", i, cpu_mhz[i],  max_cpu_mhz[i], min_cpu_mhz[i]);
+        for (i = 0; i < pcount; i++) {
+                printf("Processor [%d] Current %.0f MHz || Max %.0f || Min %.0f  \n", i, cpuf[i],  max_cpuf[i], min_cpuf[i]);
         }
-
         
-        free(cpu_mhz);
-
+        free(cpuf);
 }
